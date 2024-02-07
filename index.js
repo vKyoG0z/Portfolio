e66b88361d41e7abd02ec5d6658f80b48492e993
julien REYNAUD <julienjulienreynaud@outlook.fr>
2024-02-07 11:17:28 +0000

    Ajout de la navbar avec le style bleu (dans index donc a rangé)

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+// app.get('/navbar', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'pages', 'navbar.html'));
+// });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
