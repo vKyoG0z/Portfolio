8ea76ea5d8516faf840f229eeab3abec3940797f
julien REYNAUD <julienjulienreynaud@outlook.fr>
2024-02-13 14:44:15 +0000

    Ajout de l'image slimego qui ridirige vers projet/slimego (l'image ne s'affiche pas et le style de la navbar n'est pas actif sur la page slimego.html)

--- index.js
+++ index.js
@@ -6,23 +6,28 @@ const port = 3010;
 
 app.use(express.static('static'));
 
-// Définir la route pour les pages HTML
+// Route racine du projet qui redirige vers index.html
 app.get('/', (req, res) => {
-  // Renvoyer la page index.html avec la barre de navigation incluse
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
 // Définition de la route pour la barre de navigation
 app.get('/navbar', (req, res) => {
-  // Renvoyer la barre de navigation (navbar.html) avec l'identifiant de la page actuelle
-  let currentPage = req.query.page || 'accueil'; // Page par défaut : accueil
+  // Renvoyer la barre de navigation navbar.html avec l'identifiant de la page actuelle
+  let currentPage = req.query.page || 'accueil'; // La page par defaut est accueil
   res.sendFile(resolve(__dirname, 'pages/navbar.html'));
 });
 
-//Définition de la route pour la page projet
+// Définition de la route pour la page projet
 app.get('/projet', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/projet.html'));
 });
+
+// Définition de la route pour la page slimego (sous-page de projet)
+app.get('/projet/slimego', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/slimego.html'));
+});
+
 
 //Définition de la route pour la page formation
 app.get('/formation', (req, res) => {
