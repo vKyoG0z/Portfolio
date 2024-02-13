6e739693acd8fccc40418c8aa7acd39f0be3b324
julien REYNAUD <julienjulienreynaud@outlook.fr>
2024-02-13 13:46:53 +0000

    Ajout de la nav bar sur toutes les pages et redirection vers les pages via la nav bar

--- index.js
+++ index.js
@@ -6,13 +6,33 @@ const port = 3010;
 
 app.use(express.static('static'));
 
+// Définir la route pour les pages HTML
 app.get('/', (req, res) => {
+  // Renvoyer la page index.html avec la barre de navigation incluse
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-// app.get('/navbar', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'pages', 'navbar.html'));
-// });
+// Définition de la route pour la barre de navigation
+app.get('/navbar', (req, res) => {
+  // Renvoyer la barre de navigation (navbar.html) avec l'identifiant de la page actuelle
+  let currentPage = req.query.page || 'accueil'; // Page par défaut : accueil
+  res.sendFile(resolve(__dirname, 'pages/navbar.html'));
+});
+
+//Définition de la route pour la page projet
+app.get('/projet', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/projet.html'));
+});
+
+//Définition de la route pour la page formation
+app.get('/formation', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/formation.html'));
+});
+
+//Définition de la route pour la page contact
+app.get('/contact', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/contact.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
